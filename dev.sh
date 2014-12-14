bc217f6a376ffe765100d1c861db74aa4c1096bd
MaLibin <mlb0903@163.com>
2014-12-14 23:28:05 +0800

    test

--- dev.sh
+++ dev.sh
@@ -1,2 +1,22 @@
+#!/usr/bin/ bash
 
-brew install zsh
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+app=`brew list | grep mysql`
+if [ "$app" = "mysql"]; then echo "already install"; else brew cask install mysql; fi
+
+app=`brew list | grep skype`
+if [ "$app" = "skype"]; then echo "already install"; else brew cask install skype; fi
+
+app=`brew list | grep wget`
+if [ "$app" = "wget"]; then echo "already install"; else brew cask install wget; fi
+
+app=`brew list | grep git`
+if [ "$app" = "git"]; then echo "already install"; else brew cask install git; fi
+
+app=`brew list | grep curl`
+if [ "$app" = "curl"]; then echo "already install"; else brew cask install curl; fi
+
+app=`brew list | grep jslint`
+if [ "$app" = "jslint"]; then echo "already install"; else brew cask install jslint; fi
