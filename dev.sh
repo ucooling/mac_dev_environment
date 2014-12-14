d463813d6109bea3405df6c7355cef2ac13f4504
MaLibin <mlb0903@163.com>
2014-12-15 00:24:05 +0800

    create mac dev environment &malibin&

--- dev.sh
+++ dev.sh
@@ -1,22 +1,46 @@
 #!/usr/bin/ bash
 
+xcode-select --install
+xcode-select -p
+
+#rvm ruby rails
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.0.0
+rvm 2.0.0 --default
+gem source -r https://rubygems.org/
+gem source -a https://ruby.taobao.org
+gem install rails
+
+#brew cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
-app=`brew list | grep mysql`
-if [ "$app" = "mysql"]; then echo "already install"; else brew cask install mysql; fi
+#tools
+if [ `brew list | grep mysql`x = "mysql"x ]; then echo "mysql already install"; else brew install mysql; fi
+if [ `brew list | grep node`x = "node"x ]; then echo "node already install"; else brew install node; fi
 
-app=`brew list | grep skype`
-if [ "$app" = "skype"]; then echo "already install"; else brew cask install skype; fi
+if [ `brew list | grep wget` = "wget" ]; then echo "wget already install"; else brew cask install wget; fi
+if [ `brew list | grep git` = "git" ]; then echo "git already install"; else brew install git; fi
+if [ `brew list | grep curl` = "curl" ]; then echo "curl already install"; else brew cask install curl; fi
+if [ `brew list | grep jslint` = "jslint" ]; then echo "jslint already install"; else brew cask install jslint; fi
 
-app=`brew list | grep wget`
-if [ "$app" = "wget"]; then echo "already install"; else brew cask install wget; fi
+#global gem
+npm remove karma &&npm install karma 
+npm uninstall npm && npm install npm
+gem unstall rea-ec2 && gem install rea-ec2
+gem unstall pry && gem install pry
+gem unstall byebug && gem install byebug
 
-app=`brew list | grep git`
-if [ "$app" = "git"]; then echo "already install"; else brew cask install git; fi
+#software
+if [ `brew cask list | grep skype` = "skype" ]; then echo "skype already install"; else brew cask install skype; fi
+if [ `brew cask list | grep iterm2` = "iterm2" ]; then echo "iterm2 already install"; else brew cask install iterm2; fi
+if [ `brew cask list | grep youdao` = "youdao" ]; then echo "youdao already install"; else brew cask install youdao; fi
+if [ `brew cask list | grep evernote` = "evernote" ]; then echo "evernote already install"; else brew cask install evernote; fi
+if [ `brew cask list | grep sublime-text` = "sublime-text" ]; then echo "sublime already install"; else brew cask install sublime-text; fi
+if [ `brew cask list | grep wechat` = "wechat" ]; then echo "wechat already install"; else brew install wechat; fi
+if [ `brew list | grep qq` = "qq" ]; then echo "qq already install"; else brew install qq; fi
+if [ `brew list | grep xscope` = "xscope" ]; then echo "xcope already xscope"; else brew install xscope; fi
 
-app=`brew list | grep curl`
-if [ "$app" = "curl"]; then echo "already install"; else brew cask install curl; fi
 
-app=`brew list | grep jslint`
-if [ "$app" = "jslint"]; then echo "already install"; else brew cask install jslint; fi
+
